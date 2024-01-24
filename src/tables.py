08530f99a0c5b4e3422a7ffff9b2ae896b14c4c8
Eduardo Azeredo <eduardo.aeredo99@gmail.com>
2024-01-24 23:22:05 +0000

    Created first scheme of class

---
+++ src/tables.py
@@ -0,0 +1,33 @@
+from typing import Literal, Union
+import pandas as pd
+
+class ActuarialTable:
+    def __init__(
+            self,
+            table: Union[
+                Literal["UP-94M"],
+                Literal["UP-94F"],
+                Literal["CSO-80"],
+                Literal["AT-83M"],
+                Literal["AT-83F"],
+                Literal["AT-2000M"],
+                Literal["AT-2000F"],
+                Literal["BRS 2010M"],
+                Literal["BRS 2010F"],
+                Literal["BRM 2010M"],
+                Literal["BRM 2010F"],
+                Literal["SP-2005M"],
+                Literal["SP-2005F"],
+                Literal["RV-85M"],
+                Literal["RV-85F"],
+                Literal["GAM-94M"],
+                Literal["GAM-94F"],
+                Literal["RP-2000M"],
+                Literal["RP-2000F"],
+                Literal["BRS-2015M"],
+                Literal["BRS-2015F"],
+                Literal["BRM-2025M"],
+                Literal["BRM-2015F"]
+            ]
+        ) -> None:
+        self.table = pd.read_csv()
